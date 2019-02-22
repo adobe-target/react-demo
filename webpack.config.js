a3360eb9a0d2019548b2aaf2e59060a583bdfbb0
Vaibhav Nigam <vanigam@adobe.com>
2019-02-22 20:41:49 +0530

    Update Webpack and other dependencies to latest

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,34 @@
 var webpack = require('webpack');
 var path = require('path');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   context: path.join(__dirname, './'),
   entry: './src/index.js',
   output: {
-    path: './public/assets',
+    path: path.join(__dirname, 'public', 'assets'),
     filename: 'js/app.js'
   },
   resolve: {
-    alias: {
-      resources: path.resolve(__dirname, 'src/resources/')
-    },
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx']
+  },
+  performance: {
+    hints: false
   },
   module: {
-    loaders: [{
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['babel-preset-es2015', 'babel-preset-react'].map(require.resolve)
-      }
-    },
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react', 'env']
+        }
+      },
       {
         test: /\.(jpg|jpeg|gif|png)$/,
         exclude: /node_modules/,
         loader: 'url-loader?limit=1024&name=images/[name].[ext]'
       }
     ]
-  },
-  plugins: [
-    new CopyWebpackPlugin([
-      {
-        from: 'resources/**/*'
-      }
-    ])
-  ]
+  }
 };
